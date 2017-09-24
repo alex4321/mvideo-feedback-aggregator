9a7007da7b38427db8af347426214f598e01b32a
alex4321 <gaussmake@gmail.com>
2017-09-24 12:58:18 +0300

    Changed extractor demo to work with source dataset generally

--- extractor_demo.py
+++ extractor_demo.py
@@ -5,14 +5,19 @@ import sys
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 4, "Need trained word2vec path / dataset path / product id"
+    assert len(sys.argv) == 5, "Need trained word2vec path / dataset path / product id  / max ngrams per tfidf"
     w2v_path = sys.argv[1]
     w2v = KeyedVectors.load_word2vec_format(w2v_path, binary=True)
     df_path = sys.argv[2]
     df = pd.read_csv(df_path)
     product_id = int(sys.argv[3])
-    texts = df.loc[df["product"] == product_id, "text"]
+    max_ngram_per_tfidf = int(sys.argv[4])
+    index = df["PRODUCT"] == product_id
+    texts = list(df.loc[index, "TEXT"]) + \
+            list(df.loc[index, "BENEFITS"]) + \
+            list(df.loc[index, "DRAWBACKS"])
+    texts = list(map(str, filter(bool, texts)))
     extractor = Extractor()
     extractor.word2vec = w2v
-    extracted = extractor.transform(texts, 1, 4, 50)
+    extracted = extractor.transform(texts, 1, 4, max_ngram_per_tfidf)
     print(extracted)
