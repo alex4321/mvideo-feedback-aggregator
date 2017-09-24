26abe40e3dd2b286abdb0ed4430d8f0b6be71a65
alex4321 <gaussmake@gmail.com>
2017-09-24 13:40:10 +0300

    Modified extractor demo to pass all products

--- extractor_demo.py
+++ extractor_demo.py
@@ -5,19 +5,31 @@ import sys
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 5, "Need trained word2vec path / dataset path / product id  / max ngrams per tfidf"
+    assert len(sys.argv) == 5, "Need trained word2vec path / dataset path / product id (-1 to work with all)  / max ngrams per tfidf"
     w2v_path = sys.argv[1]
     w2v = KeyedVectors.load_word2vec_format(w2v_path, binary=True)
-    df_path = sys.argv[2]
-    df = pd.read_csv(df_path)
-    product_id = int(sys.argv[3])
-    max_ngram_per_tfidf = int(sys.argv[4])
-    index = df["PRODUCT"] == product_id
-    texts = list(df.loc[index, "TEXT"]) + \
-            list(df.loc[index, "BENEFITS"]) + \
-            list(df.loc[index, "DRAWBACKS"])
-    texts = list(map(str, filter(bool, texts)))
     extractor = Extractor()
     extractor.word2vec = w2v
-    extracted = extractor.transform(texts, 1, 4, max_ngram_per_tfidf)
-    print(extracted)
+    df_path = sys.argv[2]
+    df = pd.read_csv(df_path)
+    products = list(set(df["PRODUCT"]))
+    products.sort()
+    product_id = int(sys.argv[3])
+
+
+    def shor_product(product_id):
+        max_ngram_per_tfidf = int(sys.argv[4])
+        index = df["PRODUCT"] == product_id
+        texts = list(df.loc[index, "TEXT"]) + \
+                list(df.loc[index, "BENEFITS"]) + \
+                list(df.loc[index, "DRAWBACKS"])
+        texts = list(map(str, filter(bool, texts)))
+        extracted = extractor.transform(texts, 1, 4, max_ngram_per_tfidf)
+        print(product_id)
+        print(extracted)
+
+    if product_id == -1:
+        for id in products:
+            shor_product(id)
+    else:
+        shor_product(product_id)
